71e330e1982fdc593299eb74d365ce604e07de7e
Jyoti <jyotigangi@gmail.com>
2026-06-04 09:29:01 +0000

    go

--- models/jg1.sql
+++ models/jg1.sql
@@ -1,1 +1,5 @@
-select * from DE_ECOM.SCH_ECOM.EMPLOYEES
+--select * from DE_ECOM.SCH_ECOM.EMPLOYEES
+--the above is not the correct way of writing exposing the db and sources.
+--so create a properties file to have all the sources and tables configuration
+
+SELECT * FROM {{source("dbt_project","EMPLOYEES")}}
